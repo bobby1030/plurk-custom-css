e1ffa2666c7c613e7c654d88de89f02de1a84152
BobbyHo <angela.bobby@gmail.com>
2017-01-06 21:45:24 +0800

    Add gulp support and fuck Plurk's CSS security limit

---
+++ gulpfile.js
@@ -0,0 +1,20 @@
+var gulp = require('gulp');
+var replace = require('gulp-replace');
+var sass = require('gulp-sass')
+
+gulp.task('sass', function(){
+    return gulp.src('./src/main.scss')
+        .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+        .pipe(gulp.dest('./dist'));
+});
+
+// Fuck plurk's CSS security limit
+gulp.task('fuckplurk', ['sass'], function(){
+    return gulp.src('./dist/main.css')
+        .pipe(replace(/display\:/g, 'display : '))
+        .pipe(gulp.dest('./dist'));
+});
+
+gulp.task('watch', function(){
+    gulp.watch(['./src/*.scss'], ['sass', 'fuckplurk']);
+});
